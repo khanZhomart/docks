df2b85c21a41b853277f14404c7dd5c6120c88a5
khanZhomart <jtalgatulu@yandex.kz>
2022-04-07 16:27:03 +0600

    Создал класс PierHandler без имплементации

---
+++ src/main/java/com/docks/piers/PierHandler.java
@@ -0,0 +1,16 @@
+package com.docks.piers;
+
+import com.docks.models.types.ShipType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PierHandler implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(PierHandler.class);
+    private ShipType type;
+
+    @Override
+    public void run() {
+        
+    }
+}
